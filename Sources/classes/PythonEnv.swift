12361a359d81811599b2b75e8335549f0f4f71f8
SylvanSpark Engineering <ethan@sylvansparkengineering.com>
2026-05-27 20:37:55 -0500

    Improved PythonHandler/PythonEnv. Added proper python library package.

--- Sources/classes/PythonEnv.swift
+++ Sources/classes/PythonEnv.swift
@@ -8,16 +8,60 @@
 import Foundation
 
 final class PythonEnv: @unchecked Sendable {
+    static let pythonVersion = "3.11"
+    static let pythonVersionBasic = pythonVersion.replacingOccurrences(of: ".", with: "")
     
-    static let pythonHomePath = "\(DAWSON.root)/python/python3/3.11"
-    static let pythonLibPath = "\(pythonHomePath)/lib/libpython3.11.dylib"
-    static let pythonPackagesPath = "\(pythonHomePath)/lib/python3.11/site-packages"
-    static let pythonExecPath = "\(pythonHomePath)/bin/python3"
+    #if os(macOS)
+    static let pythonHome = URL(fileURLWithPath: DAWSON.root)
+        .appendingPathComponent("python-macos")
+    static let pythonLibPath = pythonHome
+        .appendingPathComponent("lib")
+        .appendingPathComponent("libpython\(pythonVersion).dylib")
+        .path
+    static let pythonPackagesPath = pythonHome
+        .appendingPathComponent("lib")
+        .appendingPathComponent("python\(pythonVersion)")
+        .appendingPathComponent("site-packages")
+        .path
+    static let pythonExecPath = pythonHome
+        .appendingPathComponent("bin")
+        .appendingPathComponent("python3")
+        .path
+    #elseif os(Windows)
+    static let pythonHome = URL(fileURLWithPath: DAWSON.root).appendingPathComponent("python-windows")
+    static let pythonLibPath = pythonHome
+        .appendingPathComponent("python\(pythonVersionBasic).dll")
+        .path
+    static let pythonPackagesPath = pythonHome
+        .appendingPathComponent("Lib")
+        .appendingPathComponent("site-packages")
+        .path
+    static let pythonExecPath = pythonHome
+        .appendingPathComponent("python.exe")
+        .path
+    #endif
     
     static func setEnv() {
+        // IMPORTANT: This function must be called before PythonKit is imported.
+        
+//        print("🐍 Python Environment Setup")
+//        print("PYTHON_VERSION:", pythonVersion)
+//        print("PYTHON_VERSION_BASIC:", pythonVersionBasic)
+//        print("PYTHONHOME:", pythonHome.path)
+//        print("PYTHON_LIBRARY:", pythonLibPath)
+//        print("PYTHONPATH:", pythonPackagesPath)
+//        print("PYTHON_EXEC:", pythonExecPath)
+        
+        #if os(macOS)
         setenv("PYTHON_LIBRARY", pythonLibPath, 1)
-        setenv("PYTHONHOME", pythonHomePath, 1)
+        setenv("PYTHONHOME", pythonHome.path, 1)
         setenv("PYTHONPATH", pythonPackagesPath, 1)
         setenv("PYTHONSAFEPATH", "1", 1)
+        #elseif os(Windows)
+        _putenv_s("PYTHON_LIBRARY", pythonLibPath)
+        _putenv_s("PYTHONHOME", pythonHome.path)
+        _putenv_s("PYTHONPATH", pythonPackagesPath)
+        _putenv_s("PYTHONSAFEPATH", "1")
+        #endif
     }
 }
